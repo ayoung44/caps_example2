847042e885ed2868546bfb0b4ef2abfc69d24b8a
ayoung44 <a_young__@naver.com>
2020-05-06 17:15:05 +0900

    fragment

--- app/src/main/java/com/example/caps_example2/HomeActivity.java
+++ app/src/main/java/com/example/caps_example2/HomeActivity.java
@@ -12,10 +12,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    // 지도, 뉴스, 게시판, 마이페이지 fragment
     private FragmentManager fragmentManager = getSupportFragmentManager();
-    private homeFragment homeFrg = new homeFragment();
-    private myPageFragment myPageFrg = new myPageFragment();
+    private mapFragment mapFrg = new mapFragment();
     private newsFragment newsFrg = new newsFragment();
+    private boardFragment boardFrg = new boardFragment();
+    private myPageFragment myPageFrg = new myPageFragment();
+
 
 
 
@@ -24,8 +27,9 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // 맨 처음 화면이 나타날 때 FrameLayout 에 표시할 fragment 를 추가함
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.frameLayout, homeFrg).commitAllowingStateLoss();
+        transaction.replace(R.id.frameLayout, mapFrg).commitAllowingStateLoss();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
@@ -38,16 +42,20 @@ public class HomeActivity extends AppCompatActivity {
             FragmentTransaction transaction = fragmentManager.beginTransaction();
 
             switch (menuItem.getItemId()) {
-                case R.id.homeItem:
-                    transaction.replace(R.id.frameLayout, homeFrg).commitAllowingStateLoss();
-                    break;
-
-                case R.id.myPageItem:
-                    transaction.replace(R.id.frameLayout, myPageFrg).commitAllowingStateLoss();
+                case R.id.mapItem:
+                    transaction.replace(R.id.frameLayout, mapFrg).commitAllowingStateLoss();
                     break;
 
                 case R.id.newsItem:
                     transaction.replace(R.id.frameLayout, newsFrg).commitAllowingStateLoss();
+                    break;
+
+                case R.id.boardItem:
+                    transaction.replace(R.id.frameLayout, boardFrg).commitAllowingStateLoss();
+                    break;
+
+                case R.id.myPageItem:
+                    transaction.replace(R.id.frameLayout, myPageFrg).commitAllowingStateLoss();
                     break;
             }
             return true;
--- app/src/main/java/com/example/caps_example2/mapFragment.java
+++ app/src/main/java/com/example/caps_example2/mapFragment.java
@@ -4,32 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class myPageFragment extends Fragment {
-
-    // Firebase
-    private FirebaseAuth mAuth = null;
-
-    // private String
-
+public class mapFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_my_page, container, false);
-
-        mAuth = FirebaseAuth.getInstance();
-//        firestore = FirebaseFirestore.getInstance();
-
-
-        return view;
-
-
+        return inflater.inflate(R.layout.fragment_map, container, false);
     }
 }
